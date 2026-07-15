cd501fb697c0242c9ff9a88fbedc885021969948
Aman Singh Rana <107622127+aman7cr74@users.noreply.github.com>
2026-07-15 12:13:06 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from aman 🚀"
+    return "Hello from aman rana , aa tera baap aaya🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
